e3e59d08d8d272c2f0d82bbed5ce96a70bd6bdb1
Richard <legalize@xmission.com>
2014-11-09 07:32:19 -0700

    Refactor: Extract JSON grammar class

--- json.cpp
+++ json.cpp
@@ -2,19 +2,44 @@
 #include "json.h"
 #include <boost/spirit/include/qi.hpp>
 
+using namespace boost::spirit::qi;
+
+namespace
+{
+
+typedef ascii::space_type skipper;
+
+template <typename Iter>
+struct json_grammar : grammar<Iter, json::value(), skipper>
+{
+    json_grammar() : json_grammar::base_type(start)
+    {
+        boolean = bool_;
+        integer = int_ >> !no_case[char_(".e")];
+        number = double_;
+        quoted_string = '"' >> *(char_ - '"') >> '"';
+        start = boolean | integer | number | quoted_string;
+    }
+
+    rule<Iter, json::value(), skipper> start;
+    rule<Iter, bool(),        skipper> boolean;
+    rule<Iter, int(),         skipper> integer;
+    rule<Iter, double(),      skipper> number;
+    rule<Iter, std::string(), skipper> quoted_string;
+};
+
+}
+
 namespace json
 {
 
 value parse(std::string const& text)
 {
-    using namespace boost::spirit::qi;
     std::string::const_iterator start{text.begin()};
     value result;
     if (phrase_parse(start, text.end(),
-            bool_
-            | (int_ >> !no_case[char_(".e")])
-            | double_
-            | ('"' >> *(char_ - '"') >> '"'), ascii::space, result)
+            json_grammar<std::string::const_iterator>(),
+            ascii::space, result)
         && start == text.end())
     {
         return result;
